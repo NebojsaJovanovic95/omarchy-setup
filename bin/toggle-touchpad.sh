0978137a670d03665a1c660fbdb04072fadda8a7
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2025-12-21 17:37:49 -0500

    added apple laptop fallback

--- bin/toggle-touchpad.sh
+++ bin/toggle-touchpad.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 # Elan touchpad device name
-DEVICE=$(hyprctl devices | grep -i touchpad | xargs)
-
+DEVICE=$(hyprctsl devices | grep -i touchpad | xargs)
+if [ -z "$DEVICE" ]; then
+  DEVICE=$(hyprctl devices | awk '/mice:/{f=1} f && /bcm5974/{print "bcm5974"; exit}')
+fi
 # state file
 STATEFILE="$HOME/.cache/touchpad.state"
 # Default to ON if no state file exists
