1eb2ba3f7da81b46f386b1e9b38b6f6908843734
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2025-12-26 21:21:19 -0500

    scrolling is bullshit, i removed it

--- bin/run-battlenet.sh
+++ bin/run-battlenet.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+export STEAM_COMPAT_CLIENT_INSTALL_PATH="/home/nebojsa/.steam/steam"
+export STEAM_COMPAT_DATA_PATH="/home/nebojsa/.steam/steam/steamapps/compatdata/2906910801"
+
 PROTON="/home/nebojsa/.steam/steam/steamapps/common/Proton - Experimental/proton"
-PREFIX="/home/nebojsa/.steam/steam/steamapps/compatdata/2906910801/pfx"
-"$PROTON" run --prefix "$PREFIX" "$PREFIX/drive_c/Program Files (x86)/Battle.net/Battle.net Launcher.exe"
+
+"$PROTON" run \
+"/home/nebojsa/.steam/steam/steamapps/compatdata/2906910801/pfx/drive_c/Program Files (x86)/Battle.net/Battle.net Launcher.exe"
