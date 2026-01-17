81040c34ad55939b3a2f42f72b3a43dc732b6b18
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-01-17 18:52:30 -0500

    alacritty and hyprland configs updated

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 rsync -a ./bin/. "$HOME/.local/bin/"
 rsync -a ./config/hypr/ "$HOME/.config/hypr/"
+rsync -a ./config/alacritty/ "$HOME/.config/alacritty/"
 rsync -a ./desktop_apps/ "$HOME/.local/share/applications/"
 rsync -a ./icons/ "$HOME/.local/share/icons/"
