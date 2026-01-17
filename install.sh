a983342c019f65537d6d1ddddf1fe6f4384f77b6
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-01-17 13:03:43 -0500

    added icons

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 rsync -a ./bin/. "$HOME/.local/bin/"
 rsync -a ./config/hypr/ "$HOME/.config/hypr/"
 rsync -a ./desktop_apps/ "$HOME/.local/share/applications/"
+rsync -a ./icons/ "$HOME/.local/share/icons/"
